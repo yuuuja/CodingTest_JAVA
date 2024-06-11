2c9f8bb56b5ba5b512d82c388a8160e4f33ac930
Yujeong <155720162+yuuuja@users.noreply.github.com>
2024-06-11 15:44:21 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(int i=0;i<order.length;i++){
+            if(order[i].contains("latte")){
+                answer+=5000;
+            } else{
+                answer+=4500;
+            }
+        }
+        return answer;
+    }
+}
