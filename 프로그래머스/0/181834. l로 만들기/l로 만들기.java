a36b7a4a3836213677ee345080fbbd8f73eec413
Yujeong <155720162+yuuuja@users.noreply.github.com>
2024-06-11 14:27:12 +0900

    [level 0] Title: l로 만들기, Time: 629.46 ms, Memory: 390 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,12 +1,14 @@
 class Solution {
     public String solution(String myString) {
-        char[] c = myString.toCharArray();
+        
         String answer="";
-        for(int i=0;i<c.length;i++){
-            if(c[i]<'l'){
-                c[i]='l';
+        for(char c:myString.toCharArray()){
+            if(c<'l'){
+                answer+='l';
+            } else{
+                answer +=c;
             }
-            answer +=c[i];
+            
         }
         
         return answer;
