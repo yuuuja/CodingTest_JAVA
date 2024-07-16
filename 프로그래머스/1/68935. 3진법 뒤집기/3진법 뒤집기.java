93bbc01a2ec6cdc0902a5a84960df67a928a8299
Yujeong <155720162+yuuuja@users.noreply.github.com>
2024-07-17 01:55:28 +0900

    [level 1] Title: 3진법 뒤집기, Time: 1.94 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String trit = "";
+        while(n != 0){
+            trit += String.valueOf(n%3);
+            n/=3;
+            if(n==1){
+                trit+="1";
+                break;
+            }
+        }
+
+        for(int i=0;i<trit.length();i++){
+            answer +=  (int)Math.pow(3,i) * Integer.valueOf(trit.charAt(trit.length()-1-i)-48);
+        }
+
+        return answer;
+    }
+}
