81d6433b4252fa7ec31d95502fcde99911a63e5b
Yujeong <155720162+yuuuja@users.noreply.github.com>
2024-07-19 20:15:52 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        int idx=0;
+        for(int i=1;i<=n;i++){
+            if(i%2==1){
+                answer[idx]=i;
+                idx++;
+            }
+        }
+        return answer;
+    }
+}
