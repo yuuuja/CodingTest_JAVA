c51e9885e597ab66950925d2823073b621a14268
Yujeong <155720162+yuuuja@users.noreply.github.com>
2024-08-04 14:32:00 +0900

    [level 1] Title: 기사단원의 무기, Time: 9.73 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,16 +1,19 @@
 class Solution {
+
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        
-        for(int i=1;i<=number;i++){
-            int count = 0; // 초기화
-            for(int j=1;j*j<=i;j++){
-                if(j*j == i) //제곱수이면 약수의 개수는 홀수
-                    count++;
-                else if(i % j == 0)
-                    count += 2;
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            answer += count > limit ? power : count;
+        }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
         }
         return answer;
     }
