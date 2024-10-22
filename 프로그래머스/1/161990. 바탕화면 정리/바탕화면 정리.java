36a4271e4c0b96b63c80bdcd1c150e043184f5c3
Yujeong <155720162+yuuuja@users.noreply.github.com>
2024-10-22 16:06:07 +0900

    [level 1] Title: 바탕화면 정리, Time: 6.47 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        List<Point> points = new ArrayList<>();
+        int minX=51; int minY=51;
+        int maxX=0; int maxY=0;
+        
+        // 파일 좌표 리스트에 저장
+        for(int i=0;i<wallpaper.length;i++){
+            String[] str = wallpaper[i].split("");
+            for(int j=0;j<str.length;j++){
+                if(str[j].equals("#")){
+                    points.add(new Point(j,i));
+                }
+            }
+        }
+        // 최대, 최소 x좌표, y좌표 구하기
+        for(Point point : points){
+            if (point.getX() < minX) minX = point.getX();
+            if (point.getX() > maxX) maxX = point.getX();
+            if (point.getY() < minY) minY = point.getY();
+            if (point.getY() > maxY) maxY = point.getY();
+        }
+          
+        int[] answer = {minY, minX, maxY+1, maxX+1};
+        return answer;
+    }
+}
+class Point{
+    private int x;
+    private int y;
+    
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+}
