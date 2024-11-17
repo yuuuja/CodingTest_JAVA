789e6bf664a14c50e95e129bafbb90a9c7d67b28
Yujeong <155720162+yuuuja@users.noreply.github.com>
2024-11-17 17:45:09 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
+        int[] answer = new int[2]; //[h,w]
         int total = brown + yellow;
-        for(int w = 3; w<=total/3;w++){
+        for(int w = 3; w <= (int) Math.sqrt(total); w++){ //최소 3부터 시작
             if(total % w == 0){
                 int h = total / w;
                 if((w-2)*(h-2)==yellow){
