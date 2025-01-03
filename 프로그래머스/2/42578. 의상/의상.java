03ccab6ab43dfbc079de7b69695d30f04019da71
Yujeong <155720162+yuuuja@users.noreply.github.com>
2025-01-03 17:17:00 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,7 +6,7 @@ class Solution {
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1); // 의상 종류 별로 몇 개 있는지 
         }
         int answer = 1;
-        for(String key : map.keySet()){ //조합
+        for(String key : map.keySet()){ 
             answer *= (map.get(key)+1);// 안입는 경우 고려해서 +1
         }
         answer-=1; // 최소 한개 의상은 입으므로 모두 안입는 경우 제외
