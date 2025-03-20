9ffaf0fdfb10e9a60cf167aafeb1030c329b9078
Yujeong <155720162+yuuuja@users.noreply.github.com>
2025-03-20 17:55:33 +0900

    [level 2] Title: 소수 찾기, Time: 29.93 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,32 +1,35 @@
 import java.util.*;
 class Solution {
     public int solution(String numbers) {
-        Set<Integer> set = new HashSet<>();
         int answer = 0;
-        permutations("",numbers,set);
+        Set<Integer> set = new HashSet<>();
         
-        for(int num : set){
-            if(isPrime(num)){ // 소수라면 1증가
+        permutation("",numbers,set);
+        
+        for(int num: set){
+            if(isPrime(num)){
                 answer++;
             }
         }
         return answer;
     }
-    private void permutations(String prefix, String remaining, Set<Integer> set){
-        if(!prefix.isEmpty()){
-            set.add(Integer.parseInt(prefix));
+    // 순열 
+    private void permutation(String str, String numbers, Set<Integer> set){
+        if(!str.isEmpty()){
+            set.add(Integer.parseInt(str));
         }
-        //remaining 문자열을 순회하면서 각 문자를 prefix에 추가하고, 남은 문자열로 재귀 호출
-        for(int i=0; i<remaining.length();i++){
-            permutations(prefix+remaining.charAt(i),remaining.substring(0,i)+remaining.substring(i+1),set);
+        for(int i=0;i<numbers.length();i++){
+            // 한문자씩 str에 추가하고, 남은 문자열로 재귀 호출
+            permutation(str+numbers.charAt(i), numbers.substring(0,i)+numbers.substring(i+1), set); 
         }
     }
-    private boolean isPrime(int num){ // 소수 판별
+    
+    private boolean isPrime(int num){
         if(num <= 1) return false;
-        if(num == 2) return true;
-        if(num % 2 == 0) return false;
-        for(int i=3;i*i <= num;i+=2){ //홀수만 검사, i가 num의 제곱근 이하일 때만 반복
-            if(num % i == 0) return false;
+        if(num == 2 || num == 3) return true;
+        if(num % 2==0 ||num %3==0) return false;
+        for(int i=5;i*i<=num;i+=6){
+            if(num % i==0 || num % (i+2) ==0) return false;
         }
         return true;
     }
